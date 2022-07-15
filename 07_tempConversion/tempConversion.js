7002854599c1c0c1bdf8060f6ba05ff6cc5d97ae
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-15 12:18:05 +0200

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ROUND_TO_PLACES = 1;
 
+const round = function(number, places) {
+    const shift = 10 ** places;
+    return Math.round(number * shift) / shift;
 };
 
-const ctof = function() {
+const ftoc = function(fahrenheit) {
+    return round((fahrenheit - 32) * 5 / 9, ROUND_TO_PLACES);
+};
 
+const ctof = function(celsius) {
+    return round(celsius * 9 / 5 + 32, ROUND_TO_PLACES);
 };
 
 // Do not edit below this line
