b7ebf4c51c2f331a17754d7ea53f154500523872
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-15 11:58:10 +0200

    Pass first 3 tests on exercise 04

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(n, m) {
+    if (n === m) return m;
+    if (n > m) [n, m] = [m, n];
+    return sumAll(n, m - 1) + m;
 };
 
 // Do not edit below this line
