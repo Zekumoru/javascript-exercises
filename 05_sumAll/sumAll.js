0b6d73a54bc9530035b76fb673b15287c9bf38d0
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-15 12:06:37 +0200

    Finish exercise 04

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,7 @@
+const NUMBER = 'number';
+
 const sumAll = function(n, m) {
+    if (n < 0 || m < 0 || typeof(n) !== NUMBER || typeof(m) !== NUMBER) return 'ERROR';
     if (n === m) return m;
     if (n > m) [n, m] = [m, n];
     return sumAll(n, m - 1) + m;
