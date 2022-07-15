698f06ac7545daa922ae6ef617782b36e15dbe21
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-15 11:47:13 +0200

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) return 'ERROR';
+    const output = [];
+    for (let i = 0; i < times; i++) output.push(string);
+    return output.join('');
 };
 
 // Do not edit below this line
