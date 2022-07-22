d6e6311db4f63540a891bdc2ae4bd6b5e8057fbc
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-22 18:18:01 +0200

    Finish 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const letters = string.match(/[A-Z]/gi).map((letter) => letter.toUpperCase());
+    for (let i = 0, j = letters.length - 1; i <= j; i++, j--) {
+        if (letters[i] !== letters[j]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
