3132e21c1e8713e4293d6e072aa3bef0364ad611
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-22 19:36:11 +0200

    Finish 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (oldest === undefined) return person;
+        const calcAge = (person) => (person.yearOfDeath ?? (new Date().getFullYear())) - person.yearOfBirth;
+        return (calcAge(oldest) > calcAge(person))? oldest : person; 
+    });
 };
 
 // Do not edit below this line
